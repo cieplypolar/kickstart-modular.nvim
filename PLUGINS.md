431ae76460d13a95029d8074f426b8fbab330400
Szymon Szulc <szymon.szulc@student.uj.edu.pl>
2025-03-24 02:30:18 +0100

    simple config: colorscheme, keymap, fe

---
+++ PLUGINS.md
@@ -0,0 +1,26 @@
+# List of nvim plugins
+
+## I know what are they doing
+- lazy
+- gitsigns
+- mini (almost)
+- autopairs
+- todo-comments
+- which-key
+- vim-sleuth
+- rose-pine
+- neoscroll
+- oil
+
+## no idea why I need them
+- conform
+- lint
+- treesitter
+- lsp
+- indent_line
+- telescope
+- alpha
+- colorizer
+- wakatime
+- debug
+- harpoon??
---
+++ lua/plugins/rosepine.lua
@@ -0,0 +1,17 @@
+return {
+  'rose-pine/nvim',
+  enabled = true,
+  lazy = false,
+  name = 'rose-pine',
+  config = function()
+    ---@diagnostic disable-next-line: missing-fields
+    require('rose-pine').setup {
+      variant = 'main',
+      dark_variant = 'main',
+      styles = {
+        transparency = true,
+      },
+    }
+    vim.cmd.colorscheme 'rose-pine-main'
+  end,
+}
